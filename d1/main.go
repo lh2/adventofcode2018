0ff0896a7f5e4c39392e777367e498fd2d24fa78
Lukas Henkel <lh@entf.net>
2018-12-03 21:22:58 +0100

    Day 1 Task 2
    Also some refactoring

--- d1/main.go
+++ d1/main.go
@@ -4,14 +4,47 @@ import (
 	"strconv"
 )
 
-func task1(in chan string) string {
-	chksm := 0
-	for line := range in {
-		i, err := strconv.Atoi(line)
-		if err != nil {
-			panic(err)
+func atoiChan(in chan string) chan int {
+	out := make(chan int)
+	go func() {
+		for line := range in {
+			i, err := strconv.Atoi(line)
+			if err != nil {
+				panic(err)
+			}
+			out <- i
 		}
-		chksm += i
+		close(out)
+	}()
+	return out
+}
+		
+
+func task1(in chan string) string {
+	freq := 0
+	for i := range atoiChan(in) {
+		freq += i
 	}
-	return strconv.Itoa(chksm)
+	return strconv.Itoa(freq)
+}
+
+func task2(in chan string) string {
+	list := make([]int, 0)
+	for i := range atoiChan(in) {
+		list = append(list, i)
+	}
+	
+	freqs := make(map[int]bool)
+	freq := 0
+	ok := false
+	for !ok {
+		for _, v := range list {
+			freq += v
+			if _, ok = freqs[freq]; ok {
+				break
+			}
+			freqs[freq] = true
+		}
+	}
+	return strconv.Itoa(freq)
 }
