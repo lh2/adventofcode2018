df78984f0bdb61aa7ff71e2ff7e7452cde19768f
Lukas Henkel <lh@entf.net>
2018-12-04 20:03:04 +0100

    Day 3 Task 2

--- d3/main.go
+++ d3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"regexp"
 	"strconv"
+	"fmt"
 )
 
 const CLOTH_SIZE = 1000
@@ -16,24 +17,34 @@ func mustAtoi(v string) int {
 	return i
 }
 
-func task1(in chan string) string {
+func parseCut(line string) (id, x, y, w, h int) {
+	grps := cutRegexp.FindStringSubmatch(line)
+	id = mustAtoi(grps[1])
+	x = mustAtoi(grps[2])
+	y = mustAtoi(grps[3])
+	w = mustAtoi(grps[4])
+	h = mustAtoi(grps[5])
+	return
+}
+
+func computeClothOverlap(list []string) [][]int {
 	cloth := make([][]int, CLOTH_SIZE)
 	for i := 0; i < CLOTH_SIZE; i++ {
 		cloth[i] = make([]int, CLOTH_SIZE)
 	}
-	for line := range in {
-		grps := cutRegexp.FindStringSubmatch(line)
-		//id := mustAtoi(grps[1])
-		x := mustAtoi(grps[2])
-		y := mustAtoi(grps[3])
-		w := mustAtoi(grps[4])
-		h := mustAtoi(grps[5])
+	for _, line := range list {
+		_, x, y, w, h := parseCut(line)
 		for xp := x; xp < x + w; xp++ {
 			for yp := y; yp < y + h; yp++ {
 				cloth[xp][yp]++
 			}
 		}
 	}
+	return cloth
+}
+
+func task1(in chan string) string {
+	cloth := computeClothOverlap(inAsSlice(in))
 	cntInches := 0
 	for _, row := range cloth {
 		for _, cell := range row {
@@ -43,4 +54,24 @@ func task1(in chan string) string {
 		}
 	}
 	return strconv.Itoa(cntInches)
+}
+
+func task2(in chan string) string {
+	list := inAsSlice(in)
+	cloth := computeClothOverlap(list)
+	uniqueId := -1
+	for _, line := range list {
+		id, x, y, w, h := parseCut(line)
+		hasOverlap := false
+		for xp := x; !hasOverlap && xp < x + w; xp++ {
+			for yp := y; !hasOverlap && yp < y + h; yp++ {
+				hasOverlap = cloth[xp][yp] > 1
+			}
+		}
+		if !hasOverlap {
+			uniqueId = id
+			break
+		}
+	}
+	return strconv.Itoa(uniqueId)
 }
