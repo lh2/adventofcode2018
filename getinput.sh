8c340e442630acc3595254c79f1a52ca4fb1d660
Lukas Henkel <lh@entf.net>
2018-12-04 22:13:07 +0100

    Create input folder if it does not exist and fail on error

--- getinput.sh
+++ getinput.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 if [ ! -e session.txt ]; then
 	echo 1>&2 Please create a \"session.txt\" file containing your advent of code HTTP cookie
 	exit 1
@@ -7,6 +8,9 @@ if [ "x$1" != "x" ]; then
 	day=$1
 else
 	day=`date +%-d`
+fi
+if [ ! -d input ]; then
+	mkdir input
 fi
 
 curl -b "session=`cat session.txt`" https://adventofcode.com/2018/day/$day/input > input/$day.txt
