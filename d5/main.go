3a82fcc138938b7466b32c9c226224f6ca5fc728
Lukas Henkel <lh@entf.net>
2018-12-05 18:29:40 +0100

    Day 5 Task 2

--- d5/main.go
+++ d5/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -17,7 +18,7 @@ func reactPolymer(polymer string) (result string, reactions int) {
 			reactions++
 			i++
 		} else {
-			result += string(polymer[i])
+			result += string(runes[i])
 		}
 
 	}
@@ -27,11 +28,33 @@ func reactPolymer(polymer string) (result string, reactions int) {
 	return
 }
 
-func task1(in chan string) string {
-	polymer := <-in
+func fullyReactPolymer(polymer string) string {
 	reactions := 1
 	for reactions > 0 {
 		polymer, reactions = reactPolymer(polymer)
 	}
+	return polymer
+}
+
+func task1(in chan string) string {
+	polymer := fullyReactPolymer(<-in)
 	return strconv.Itoa(len(polymer))
+}
+
+func task2(in chan string) string {
+	polymer := <-in
+	shortest := int(^uint(0) >> 1)
+	for unit := 'a'; unit <= 'z'; unit++ {
+		r := strings.NewReplacer(
+			string(unit), "",
+			string(unicode.ToUpper(unit)), "")
+		p := r.Replace(polymer)
+
+		p = fullyReactPolymer(p)
+		pl := len(p)
+		if pl < shortest {
+			shortest = pl
+		}
+	}
+	return strconv.Itoa(shortest)
 }
